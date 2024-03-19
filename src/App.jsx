502abb44315edb953ca28af0db0956b55254992e
Darren Oday <darren.oday@gmail.com>
2024-03-18 20:35:56 -0500

    finished assignment 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from"./components/Banner";
+import {useState} from "react";
+import satData from "./components/satData";
+import styles from "./components/styling.css"
+
 
 
 function App() {
+ 
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new
+    Set(satData.map((data)=>data.orbitType))];
+  const filterByType = (currentType) => {
+      const displaySats = satData.filter((newSatDisplay) => {
+         return newSatDisplay.orbitType === currentType;
+      });
+      setSat(displaySats);
+   };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons
+       filterByType={filterByType}
+       setSat={setSat} 
+       displaySats={displaySats}
+       />
+      <Table sat={sat}/>
     </div>
+   
   );
+  
 }
 
 export default App;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,37 @@
+import styles from "./styling.css"
+
 const Table = ({ sat }) => {
+
+  const renderTableData=()=>{
+    
+    return sat.map((data) => (
+      <tr key={data.id}> 
+      <td>{data.name}</td>
+      <td>{data.type}</td>
+      <td>{data.launchDate}</td>
+      <td>{data.operational ? 'active' : 'inactive'}
+      </td>
+      </tr>
+      
+    ));
+  };
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
+     {renderTableData()}
      <tr>
-       <td>Row Data TBD</td>
-     </tr>
+    </tr>
      </tbody>
    </table>
   );
-};
+}
 
 export default Table;
